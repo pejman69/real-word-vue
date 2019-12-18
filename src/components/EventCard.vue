1d719b1447ce52cd4025b880850fcf0938990012
pejmanmowloudi <pejmanmowloudi@gmail.com>
2019-12-18 15:49:54 +0330

    Finish V 6

---
+++ src/components/EventCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <router-link class="event-link" :to="{ name: 'event-show', params: { id: '1'}}">
+  <div class="event-card -shadow">
+    <spen class="eyebrow">@{{ event.time }} on {{ event.date }}</spen>
+  <h4 class="title">{{ event.title }}</h4>
+  <spen>{{ event.attendees.length }} attending</spen>
+  </div>
+  </router-link>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        event: {
+          id: 1,
+          title: 'Beach Cleanup',
+          date: ' Tuse Agus 19, 2019',
+          time: '60:00',
+          attendees: [
+            { id: 'abc12', name: 'pejman mowloudi'},
+            { id: 'abc123', name: 'peshawa mowloudi'}
+          ]
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
